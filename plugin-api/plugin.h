9f82ad7650d23434054ea79280642d429ea67e90
fritzone <fritzone@gmail.com>
2013-03-27 12:29:37 +0100

    SINF0000 more plugin design

--- plugin-api/plugin.h
+++ plugin-api/plugin.h
@@ -2,20 +2,10 @@
 #define PLUGIN_H
 
 /**
- * The definition of a plugins' exposed interface towards the application. This
- * header file contains all the methods that the plugins of the components
- * should expose to the outside world, but you need to implement only the
- * methods that are relevant to the plugin you develop.
- *
- * For Client plugins the following methods should be implemented:
- *
- *  1. @see int load();
- *
- *  2. @see PLUGIN_COMPONENT component();
- *     Always should return PLUGIN_CLIENT
- *
- *  3. @see const char* execute();
- *
+ * The definition of a client plugins' exposed interface towards the application
+ * A client plugin is linked into a dynamic library (.so) and loaded upon start
+ * up of the application. The methods you implement below are called in the
+ * specific stage in the life cycle of the application.
  */
 
 #ifdef __cplusplus
@@ -35,7 +25,11 @@ extern "C" {
  *                  <plugin name=@see name()>
  *                    ... HERE GOES THE RETURN VALUE OF THE execute() METHOD ...
  *                  </plugin>
-  *
+ *
+ *                  This method is called at regular intervals while running,
+ *                  this value is specified in the "frequency" setting of the
+ *                  specific plugin.
+ *
  * @param free_returned_value - how to deal with the data that is returned.
  *                  The following values are possible:
  *
@@ -44,13 +38,18 @@ extern "C" {
  *                  DELETE_ME - free the allocated memory with delete []
  *
  * @return - the gathered data formatted for the core side component. If this
- *           return NULL or empty string it is ignored by the caller.
+ *                  returns NULL or empty string it is ignored by the caller.
  */
 char* execute(ALLOCATION_BEHAVIOR* free_returned_value);
 
 /**
  * @brief component - returns the @see PLUGIN_COMPONENT of this plugin (ie: the
- *        type of the plugin).
+ *                    type of the plugin). This is just a security check, to
+ *                    make it possible to prevent the loading of non client
+ *                    plugins into the client component.
+ *
+ *                    This method for the client plugins should always return
+ *                    the PLUGIN_CLIENT value.
  *
  * @return the @see PLUGIN_COMPONENT of this plugin
  */
@@ -66,6 +65,8 @@ PLUGIN_COMPONENT component();
  *         PLUGIN_LOAD_FAILED - in case the plugin failed to load.
  *         PLUGIN_LOAD_SYSTEM_FAILURE - in case the system cannot continue
  *                         loading.
+ *
+ *         @see PLUGIN_LOAD_STATUS for a more detailed description of the values
  *
  *         Any other value will be treated with an ERROR level entry in the log
  *         file and the plugin will not be added to the loaded plugins (ie: it
@@ -83,14 +84,47 @@ PLUGIN_LOAD_STATUS load();
  */
 PLUGIN_UNLOAD_STATUS unload(PLUGIN_UNLOAD_REQUEST reason);
 
-// an internal about string. User should NOT free the data returned from here
+/**
+ * @brief about - Returns an "about" string, such as "Plugin (c) 2012 XYZ."
+ *                The caller will NOT free the data returned from here, so
+ *                usually here you should return a const static char* string.
+ *
+ * @return the about string of the plugin
+ */
 const char* about();
 
-// the name of the plugin. User should NOT free this
+/**
+ * @brief name - returns the name of the plugin. This name should be the same as
+ *               in the configuration file.
+ *
+ * @return the name of the plugin
+ */
 const char* name();
 
-// does some setup work. Plugin dependent
-int setup(const char*);
+/**
+ * @brief setup - this function provides internal setup functionality for the
+ *                plugin. Implement this as per your requirements of the
+ *                interpretation of the commands. Return any value you consider
+ *                relevant to be passed on to the caller.
+ *
+ *                The typical workflow for this functionality is:
+ *
+ *                1. In the web gui the user selects the host, and there it sees
+ *                   the plugins the client component of that host has.
+ *                2. The web gui provides a list of the plugin specific options
+ *                   the user can choose from (such as for the disk status
+ *                   plugin: do not scan this hard drive for a speicifc hard
+ *                   disk) and the user chooses the option
+ *                3. The web-gui renders the user selected option into a
+ *                   command string (such as: DO_NOT_SCAN /dev/sdb1)
+ *                4. This command is sent to the client side of the
+ *                   plugin triumvirate and this setup() method takes care of
+ *                   the action.
+ *
+ * @return any value you consider to be useful and which can be handled by the
+ *         web gui.
+ */
+int setup(const char* commands);
 
 /*****************************************************************************
  *             Typedefs used for the function pointers                       *

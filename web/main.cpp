2df64d18a051ba0616de1206d6a6b3c0432dda02
fritzone <fritzone@gmail.com>
2013-04-24 15:53:10 +0200

    SINF0000 solved the one application as server problem partially

--- web/main.cpp
+++ web/main.cpp
@@ -1,22 +1,33 @@
 #include <tnt/tntnet.h>
 
+#include <cxxtools/loginit.h>
+
+log_define("sinfonifry.web")
+
 int main(int argc, char* argv[])
 {
   try
   {
-    tnt::Tntnet app;
-    app.listen("0.0.0.0", 8000);
-    app.mapUrl("^/$","sinfonifry@sinfonifry");
-    app.mapUrl("^/([^.]+)(\\..+)?","$1@sinfonifry");
-    app.mapUrl("/sinfonifry", "sinfonifry@sinfonifry");
-    app.mapUrl("/(.*).png", "images@sinfonifry");
-    app.mapUrl("/(.*).js", "jscript@sinfonifry");
-    app.mapUrl("/(.*).css", "css@sinfonifry");
+        log_init("log.properties");
 
-    app.run();
-  }
-  catch (const std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
+        tnt::Tntnet app;
+        app.listen("0.0.0.0", 8000);
+        app.mapUrl("/sinfonifry", "sinfonifry");
+        app.mapUrl("/host_list", "host_list");
+        app.mapUrl("/menu", "menu");
+
+        app.mapUrl("/add_host", "add_host");
+        app.mapUrl("/get_data", "get_data");
+        app.mapUrl("/get_host_status", "get_host_status");
+
+        app.mapUrl("/(.*).png", "images");
+        app.mapUrl("/(.*).js", "jscript");
+        app.mapUrl("/(.*).css", "css");
+
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
